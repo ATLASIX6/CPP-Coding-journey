099e0f20cd681a98f50f10d4829b4d9bd8e266c2
Atlas <ahmed.abdelli6576@gmail.com>
2026-07-26 02:36:30 +0100

    Add files via upload

---
+++ Level#3/Problems/Problem#22.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void PrintFibonacciUsingLoop(short Number)
+{
+	int FibNumber = 0;
+	int Prev2 = 0, Prev1 = 1;
+	cout << "1   ";
+	for (short i = 2; i <= Number; i++)
+	{
+		FibNumber = Prev1 + Prev2;
+		cout << FibNumber << "   ";
+		Prev2 = Prev1;
+		Prev1 = FibNumber;
+	}
+}
+
+void PrintFibonacciUsingR(short Number, int Prev2 ,int Prev1 )
+{
+	int FibNumber = 0;
+	if (Number > 0)
+	{
+		FibNumber = Prev1 + Prev2;
+		cout << FibNumber << "   ";
+		Prev2 = Prev1;
+		Prev1 = FibNumber;
+		PrintFibonacciUsingR(Number - 1, Prev2, Prev1);
+		
+	}
+	
+}
+
+int main()
+{
+	PrintFibonacciUsingR(10,0,1);
+
+	system("pause>0");
+	return 0;
+}
