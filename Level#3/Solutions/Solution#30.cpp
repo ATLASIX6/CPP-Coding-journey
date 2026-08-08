ed64401f880cc3b34f825e43da33e3c56f11c8e9
Atlas <ahmed.abdelli6576@gmail.com>
2026-08-08 22:37:22 +0100

    Add files via upload

---
+++ Level#3/Solutions/Solution#30.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include<string>
+#include<cctype>
+#include<iomanip>
+
+using namespace std;
+
+string ReadString()
+{
+	string S1;
+
+	cout << "Please Enter Your String?\n";
+	getline(cin, S1);
+
+	return S1;
+}
+
+char ReadChar()
+{
+	char C1;
+
+	cout << "\nPlease Enter a Character?\n";
+	cin >> C1;
+
+	return C1;
+}
+
+short CountLetter(string S1, char Letter)
+{
+	short Count = 0;
+	for (short i = 0; i < S1.length(); i++)
+	{
+		if (S1[i] == Letter)
+		{
+			Count++;
+		}
+	}
+	return Count;
+}
+
+int main()
+{
+	string S1 = ReadString();
+
+	char C1 = ReadChar();
+
+	cout << "\nLetter '" << C1 << "' Count = " << CountLetter(S1, C1);
+
+	system("pause>0");
+	return 0;
+}
