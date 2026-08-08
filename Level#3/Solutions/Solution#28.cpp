79c0f0d5c5894d7fc0c24dcad54b6338b36ee056
Atlas <ahmed.abdelli6576@gmail.com>
2026-08-08 22:36:43 +0100

    Add files via upload

---
+++ Level#3/Solutions/Solution#28.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include<string>
+#include<cctype>
+
+using namespace std;
+
+string ReadString()
+{
+	string S1;
+
+	cout << "Please Enter Your String?\n";
+	getline(cin, S1);
+
+	return S1;
+
+}
+
+char InvertLetterCase(char Char1)
+{
+	return isupper(Char1) ? tolower(Char1) : toupper(Char1);
+}
+
+string InvertAllStringLettersCase(string S1)
+{
+	for (short i = 0; i < S1.length(); i++)
+	{
+		S1[i] = InvertLetterCase(S1[i]);
+	}
+	return S1;
+}
+
+int main()
+{
+	string S1 = ReadString();
+
+	cout << "\nString After Inverting All Leters Case:\n";
+	S1 = InvertAllStringLettersCase(S1);
+	cout << S1 << endl;
+
+	system("pause>0");
+	return 0;
+
+}
