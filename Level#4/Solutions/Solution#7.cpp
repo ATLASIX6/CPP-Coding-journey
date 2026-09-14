b11ef9c697872b9ffedc210f76f4cde6f46cd263
Atlas <ahmed.abdelli6576@gmail.com>
2026-09-14 23:22:30 +0100

    Add files via upload

---
+++ Level#4/Solutions/Solution#7.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+short ReadYear()
+{
+	short Year = 0;
+
+	cout << "Please Enter a Year ?  ";
+	cin >> Year;
+
+	return Year;
+}
+
+short ReadMonth()
+{
+	short Month = 0;
+
+	cout << "Please Enter a Month ?  ";
+	cin >> Month;
+
+	return Month;
+}
+
+short ReadDay()
+{
+	short Day = 0;
+
+	cout << "Please Enter a Day ?  ";
+	cin >> Day;
+
+	return Day;
+}
+
+short DayOfWeakOrder(short Year, short Month, short Day)
+{
+	int A = ((14 - Month) / 12);
+	int Y = Year - A;
+	int M = Month + (12 * A) - 2;
+
+	//Gregorion
+	// 0:Sun,1:Mon,2:Tue....etc
+	int D = (Day + Y + (Y / 4) - (Y / 100) + (Y / 400) + ((31 * M) / 12)) % 7;
+
+	return D;
+}
+
+string DayName(short DayOfWeakOrder)
+{
+	string arrDayName[7] = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+	return arrDayName[DayOfWeakOrder];
+}
+
+int main()
+{
+	short Year = ReadYear();
+	short Month = ReadMonth();
+	short Day = ReadDay();
+
+	short DayOrder = DayOfWeakOrder(Year, Month, Day);
+
+	cout << "\n\nDate       :  " << Day << "/" << Month << "/" << Year << endl;
+	cout << "Day Order  :  " << DayOrder << endl;
+	cout << "Day Name   :  " << DayName(DayOrder);
+
+	system("pause>0");
+	return 0;
+}
